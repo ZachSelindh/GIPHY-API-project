5846bfca09ea452b58c0ef96f9eec1aa433c57e8
Zach Selindh <zachselindh@gmail.com>
2019-01-24 19:52:53 -0600

    Added initial visual elements.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var APIkey = "SQMK7mUuH7bYIwzAf8ESx5TGCqBavtzz";
+
+var APItag = "Karate"
+
+var APIrating = "PG-13"
+
+var URL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + 
+            APItag + "&rating=" + APIrating;
+
+console.log(URL);
+
+
