8610aa80983be71909a160037ec57ff95dd8b2e3
Zach Selindh <zachselindh@gmail.com>
2019-01-26 13:52:04 -0600

    Refined visual assets.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 var APIkey = "SQMK7mUuH7bYIwzAf8ESx5TGCqBavtzz";
 
-var APItag = "Karate"
+var APItag = ""
 
-var APIrating = "PG-13"
+var APIrating = ""
 
 var URL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + 
             APItag + "&rating=" + APIrating;
 
 console.log(URL);
+
+
 
 
