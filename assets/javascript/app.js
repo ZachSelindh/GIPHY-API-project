ff7bd25c3844ead387e08e93f5dbb7cc803ec00b
Zach Selindh <zachselindh@gmail.com>
2019-01-29 21:33:19 -0600

    Completed gif rendering functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,40 +4,49 @@ window.onload = function() {
 
 var APIkey = "SQMK7mUuH7bYIwzAf8ESx5TGCqBavtzz";
 
-var APItag = ""
-
-var APIrating = ""
-
-var URL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + 
-            APItag + "&rating=" + APIrating;
-
-
-console.log(URL);
-
 function addButton() {
     var newButton = $("<button>");
-    var searchInput = $("#input-field").val();
+    var searchInput = $("#input-field").val().trim();
     if (searchInput.length > 0) {
         console.log(searchInput);
+        $("#button-array").append(newButton);
         newButton.attr("data-source", searchInput);
         newButton.attr("class", "search-button");
         newButton.text(searchInput);
-        $("#button-array").append(newButton);
     } else {
     }
     $("#input-field").val("");
 }
 
-$("#submit-button").on("click", function(){
+function addGIFs(x) {
+    for (i = 0; i < 10; i ++) {
+        var newGIF = $("<img>");
+        var sourceURL = x.data[i].images.fixed_height_still.url;
+        newGIF.attr("src", sourceURL);
+        $("#gif-content").prepend(newGIF);
+    }
+}
+
+$(document).on("click", "#submit-button", function(){
     addButton();
 });
 
-$(".button").on("click", function(){
-    
+$(document).on("click", ".search-button", function(){
+    var titleTag = $(this).attr("data-source");
+    var APItag = titleTag.trim();
+    var APIURL = "http://api.giphy.com/v1/gifs/search?q=" + APItag + "&api_key=" + APIkey + "&limit=10";
 
+    $.ajax({
+        url: APIURL,
+        method: "GET"
+      }).done(function(response) {
+        console.log(response);
+        addGIFs(response);
+        var GIFsTitle = $("<h1>");
+        GIFsTitle.text(titleTag)
+        $("#gif-content").prepend(GIFsTitle);
+      });
 });
-
-
 };
 
 
